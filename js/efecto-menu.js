27c7b521fbb6a425922f48a32585dca3ed38b7b6
yesid <mayale121314@gmail.com>
2020-02-13 13:47:55 -0500

    pagina natural con bootstrap 4

---
+++ js/efecto-menu.js
@@ -0,0 +1,14 @@
+
+
+
+// $(function() {
+//     $(window).scroll(function() {
+//         var winTop = $(window).scrollTop();
+//         if (winTop >=30) {
+//             $("body").addCclass("sticky-header");
+
+//         }else{
+//             $("body").removeClass("sticky-header");
+//         }
+//     })
+// })
